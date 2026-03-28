3c3e3b3de2e48993a7e393764c88e2ab7c5f63fb
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-03-28 19:17:44 +0700

    feat: more augmentation option

--- src/vit/data/transforms.py
+++ src/vit/data/transforms.py
@@ -14,6 +14,25 @@ def get_train_transforms(config: Dict[str, Any]) -> transforms.Compose:
         flip_prob = aug_config['train']['flip_prob']
         transform_list.append(transforms.RandomHorizontalFlip(p=flip_prob))
     
+    if aug_config['train'].get('random_rotation', False):
+        rotation_degrees = aug_config['train']['rotation_degrees']
+        transform_list.append(transforms.RandomRotation(degrees=rotation_degrees))
+    
+    if aug_config['train'].get('random_affine', False):
+        affine_degrees = aug_config['train']['affine_degrees']
+        affine_scale = tuple(aug_config['train']['affine_scale'])
+        transform_list.append(transforms.RandomAffine(
+            degrees=affine_degrees,
+            scale=affine_scale
+        ))
+    
+    if aug_config['train'].get('random_perspective', False):
+        distortion = aug_config['train']['perspective_distortion']
+        transform_list.append(transforms.RandomPerspective(
+            distortion_scale=distortion,
+            p=0.5
+        ))
+    
     if aug_config['train']['color_jitter']:
         transform_list.append(transforms.ColorJitter(
             brightness=aug_config['train']['brightness'],
@@ -22,13 +41,20 @@ def get_train_transforms(config: Dict[str, Any]) -> transforms.Compose:
             hue=aug_config['train']['hue']
         ))
     
-    transform_list.extend([
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=aug_config['imagenet_norm']['mean'],
-            std=aug_config['imagenet_norm']['std']
-        )
-    ])
+    if aug_config['train'].get('random_grayscale', False):
+        grayscale_prob = aug_config['train']['grayscale_prob']
+        transform_list.append(transforms.RandomGrayscale(p=grayscale_prob))
+    
+    transform_list.append(transforms.ToTensor())
+    
+    if aug_config['train'].get('random_erasing', False):
+        erasing_prob = aug_config['train']['erasing_prob']
+        transform_list.append(transforms.RandomErasing(p=erasing_prob))
+    
+    transform_list.append(transforms.Normalize(
+        mean=aug_config['imagenet_norm']['mean'],
+        std=aug_config['imagenet_norm']['std']
+    ))
     
     return transforms.Compose(transform_list)
 
