e4ac565455d9c9974e9e6c98433d3de246c05027
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-06-18 11:30:22 +0700

    feat: register ParquetEmotionDataset in dataloader dispatch

--- src/vit/data/dataloader.py
+++ src/vit/data/dataloader.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader, Dataset
 
 from .datasets import EmotionDataset, BinaryEmotionDataset
 from .multimodal_dataset import MultiModalEmotionDataset
+from .parquet_dataset import ParquetEmotionDataset
 from .transforms import get_train_transforms, get_val_transforms, get_test_transforms
 
 _MULTIMODAL_TYPES = {
@@ -16,6 +17,8 @@ def _dataset_class(config: Dict[str, Any]) -> Type[Dataset]:
     dataset_type = config['data'].get('dataset_type', 'standard')
     if dataset_type == 'binary':
         return BinaryEmotionDataset
+    if dataset_type == 'parquet':
+        return ParquetEmotionDataset
     if dataset_type in _MULTIMODAL_TYPES:
         return MultiModalEmotionDataset
     return EmotionDataset
@@ -29,11 +32,49 @@ def _dataset_kwargs(config: Dict[str, Any]) -> Dict[str, Any]:
     if image_path_column is not None:
         kwargs['image_path_column'] = image_path_column
 
+    if dataset_type == 'parquet':
+        kwargs['filter_synthetic'] = config['data'].get('filter_synthetic', False)
+        return kwargs
+
     if dataset_type in _MULTIMODAL_TYPES:
         kwargs['aux_type'] = _MULTIMODAL_TYPES[dataset_type]
         kwargs['au_columns'] = config['data'].get('au_columns', [])
 
     return kwargs
+
+
+def _dataset_transform(config: Dict[str, Any], split: str) -> Any:
+    if split == 'train':
+        return get_train_transforms(config)
+    if split == 'val':
+        return get_val_transforms(config)
+    return get_test_transforms(config)
+
+
+def _build_dataset_for_split(
+    config: Dict[str, Any],
+    split: str,
+) -> Dataset:
+    dataset_type = config['data'].get('dataset_type', 'standard')
+    DatasetClass = _dataset_class(config)
+    extra = _dataset_kwargs(config)
+    transform = _dataset_transform(config, split)
+
+    if dataset_type == 'parquet':
+        parquet_key = f'{split}_parquet'
+        return DatasetClass(
+            parquet_path=config['data'][parquet_key],
+            transform=transform,
+            **extra,
+        )
+
+    return DatasetClass(
+        csv_file=config['data'][f'{split}_csv'],
+        img_dir=config['data'][f'{split}_img_dir'],
+        transform=transform,
+        **extra,
+        **_aux_csv_for_split(config, split),
+    )
 
 
 def _aux_csv_for_split(config: Dict[str, Any], split: str) -> Dict[str, Any]:
@@ -49,36 +90,9 @@ def _aux_csv_for_split(config: Dict[str, Any], split: str) -> Dict[str, Any]:
 
 
 def create_dataloaders(config: Dict[str, Any]) -> Tuple[DataLoader, DataLoader, DataLoader]:
-    train_transform = get_train_transforms(config)
-    val_transform = get_val_transforms(config)
-    test_transform = get_test_transforms(config)
-
-    DatasetClass = _dataset_class(config)
-    extra = _dataset_kwargs(config)
-
-    train_dataset = DatasetClass(
-        csv_file=config['data']['train_csv'],
-        img_dir=config['data']['train_img_dir'],
-        transform=train_transform,
-        **extra,
-        **_aux_csv_for_split(config, 'train'),
-    )
-
-    val_dataset = DatasetClass(
-        csv_file=config['data']['val_csv'],
-        img_dir=config['data']['val_img_dir'],
-        transform=val_transform,
-        **extra,
-        **_aux_csv_for_split(config, 'val'),
-    )
-
-    test_dataset = DatasetClass(
-        csv_file=config['data']['test_csv'],
-        img_dir=config['data']['test_img_dir'],
-        transform=test_transform,
-        **extra,
-        **_aux_csv_for_split(config, 'test'),
-    )
+    train_dataset = _build_dataset_for_split(config, 'train')
+    val_dataset = _build_dataset_for_split(config, 'val')
+    test_dataset = _build_dataset_for_split(config, 'test')
 
     batch_size = config['training']['batch_size']
     num_workers = config['training']['num_workers']
@@ -111,32 +125,9 @@ def create_dataloaders(config: Dict[str, Any]) -> Tuple[DataLoader, DataLoader, 
     return train_loader, val_loader, test_loader
 
 def get_dataset_info(config: Dict[str, Any]) -> Dict[str, Any]:
-    DatasetClass = _dataset_class(config)
-    extra = _dataset_kwargs(config)
-
-    train_dataset = DatasetClass(
-        csv_file=config['data']['train_csv'],
-        img_dir=config['data']['train_img_dir'],
-        transform=get_train_transforms(config),
-        **extra,
-        **_aux_csv_for_split(config, 'train'),
-    )
-
-    val_dataset = DatasetClass(
-        csv_file=config['data']['val_csv'],
-        img_dir=config['data']['val_img_dir'],
-        transform=get_val_transforms(config),
-        **extra,
-        **_aux_csv_for_split(config, 'val'),
-    )
-
-    test_dataset = DatasetClass(
-        csv_file=config['data']['test_csv'],
-        img_dir=config['data']['test_img_dir'],
-        transform=get_test_transforms(config),
-        **extra,
-        **_aux_csv_for_split(config, 'test'),
-    )
+    train_dataset = _build_dataset_for_split(config, 'train')
+    val_dataset = _build_dataset_for_split(config, 'val')
+    test_dataset = _build_dataset_for_split(config, 'test')
 
     return {
         'num_emotions': train_dataset.get_num_emotions(),
